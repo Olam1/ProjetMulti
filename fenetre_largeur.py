ebd9f4be5dc97df798bbc12a303258ee804396ec
Malo <44495944+Olam1@users.noreply.github.com>
2019-11-29 16:02:54 +0100

    fenetre
    fenetre

---
+++ fenetre_largeur.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 15:55:37 2019
+
+@author: malot
+"""
+
+import sys
+from PyQt5 import QtCore, QtGui, uic, QtWidgets
+
+
+qtCreatorFile = "user_interface.ui" # Enter file here.
+ 
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.afficher)
+    def afficher(self):
+        width=self.spinBox_width.value()
+        height=self.spinBox_height.value()
+        return width, height
+ 
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
+    
