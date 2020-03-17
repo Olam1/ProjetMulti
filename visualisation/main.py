57697a86a06759c2ea5ced5479fc2972c22bd565
Malo <44495944+Olam1@users.noreply.github.com>
2020-03-17 17:06:23 +0100

    cho

--- visualisation/main.py
+++ visualisation/main.py
@@ -48,25 +48,14 @@ class InterfaceGraphique(QtWidgets.QMainWindow, Ui_MainWindow):
         #Si un paramètre est changé on met le graphique à jour
         self.add_point.clicked.connect(self.afficher_graph)
         self.remove_point.clicked.connect(self.afficher_graph)
-        
-        self.point0spinBox.valueChanged.connect(self.afficher_graph)
         self.point1spinBox.valueChanged.connect(self.afficher_graph)
         self.point2spinBox.valueChanged.connect(self.afficher_graph)
         self.point3spinBox.valueChanged.connect(self.afficher_graph)
-        self.point5spinBox.valueChanged.connect(self.afficher_graph)
-        
-        self.point0Slider.valueChanged.connect(self.afficher_graph)
         self.point1Slider.valueChanged.connect(self.afficher_graph)
         self.point2Slider.valueChanged.connect(self.afficher_graph)
         self.point3Slider.valueChanged.connect(self.afficher_graph) 
-        self.point5Slider.valueChanged.connect(self.afficher_graph)
         
-        self.point1spinBox.setMinimum(self.point0spinBox.value())
-        self.point1spinBox.setValue(self.point0spinBox.value())
-        self.point2spinBox.setMinimum(self.point1spinBox.value())
-        self.point2spinBox.setValue(self.point1spinBox.value())
-        self.point3spinBox.setMinimum(self.point2spinBox.value())
-        self.point3spinBox.setValue(self.point2spinBox.value())
+        
         
    
   
