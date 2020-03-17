35e0af011b479763ddc31d3bb2a9aac849dbe7e6
Malo <44495944+Olam1@users.noreply.github.com>
2020-03-17 15:55:39 +0100

    comit

--- reconstruction/Methodes.py
+++ reconstruction/Methodes.py
@@ -1,5 +1,10 @@
 from PyQt5 import QtWidgets
 from PIL import Image
+import os
+import numpy as np
+import glob
+import pylab
+import tomopy
 
 #Ouvre toutes les ficheirs d'un dossier et genère un volume constitué de l'empilement de toutes les slices 2D
 def MatrixGeneration(filePath):   
@@ -14,29 +19,24 @@ def MatrixGeneration(filePath):
     k=0
     for img in filenames: #On suppose que tous les fichiers sont des tif    
         im=pylab.imread(img)
-        matrix[k,:,:] = im
-        k+=1   
-    matrix=np.rot90(matrix, axes=(0,1))     
+        matrix[:,:,k] = im
+        k+=1 
+    print("Je suis là")
+    #matrix=np.rot90(matrix, axes=(0,1))     
     #result = [matrix, w, d, h]
     matrix = tomopy.minus_log(matrix)
-    rec = tomopy.recon(matrix, tomopy.angles(103,0.,360.), algorithm='fbp')
-    print("test")
-    print(len(rec))
-    
+    rec = tomopy.recon(matrix, tomopy.angles(103,0.,360.), algorithm='fbp')    
     compteur=0
-    
     print(rec.shape)
-    
-
     for i in range(len(rec)):
         #img = Image.fromarray(rec[i], mode='F')
         #img = img.convert("L")
         #img.save(str(i) + ".png", "PNG")
         compteur=compteur+1
-        print(compteur)    
-        pylab.imshow(rec[1], cmap='gray')
-        pylab.imshow(rec[250], cmap='gray')
-        pylab.show()
+    print(compteur)    
+    pylab.imshow(rec[1], cmap='gray')
+    pylab.imshow(rec[250], cmap='gray')
+    pylab.show()
     return 0
 
 #Reçoit un dossier contenant les TIF et renvoie une matrice
@@ -52,13 +52,13 @@ def choix_fichier(self):
     #Si aucun dossier n'est sélectionné path = 0 et il ne se passe rien
     if path:
         try:
-            self.matrix, w, d, h = MatrixGeneration(path+'/*.tif')
+            MatrixGeneration(path+'/*.tif')
         except IndexError:
             return tomopy.project(tomopy.shepp3d(), tomopy.angles(180))
-        return self.matrix
+    
 
 #def main():
-#    obj = tomopy.shepp3d()
+#    obj = tomopy.shepp3d()  self.matrix, w, d, h = 
 #    ang = tomopy.angles(180)
 #    sim = tomopy.project(obj, ang)
 #    rec = tomopy.recon(sim, ang, algorithm='art')
