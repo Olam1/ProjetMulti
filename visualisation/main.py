f32fa5a54bcf8153d10c37ab55d348bab08ace7c
Malo <44495944+Olam1@users.noreply.github.com>
2020-03-17 17:38:00 +0100

    ouf

--- visualisation/main.py
+++ visualisation/main.py
@@ -14,6 +14,7 @@ Ui_MainWindow, QtBaseClass = uic.loadUiType("user_interface.ui")
 # et "Ui_MainWindow"
 class InterfaceGraphique(QtWidgets.QMainWindow, Ui_MainWindow):
 
+    from MethodesClasseIG import ajouter_points, choix_fichier, afficher, afficher_graph, retirer_points, capture, ActualiserMinMax
     #from MethodeUpdateGraph import update_graph
     #Defiition du constructeur de la classe
     def __init__(self):
@@ -26,18 +27,8 @@ class InterfaceGraphique(QtWidgets.QMainWindow, Ui_MainWindow):
         #Définition d'un attribut matrix
         self.matrix=0;
         
-        #Creation des menus et actions associées
-        extractAction = QtWidgets.QAction("&Select files", self)
-        extractAction.setShortcut("Ctrl+O")
-        extractAction.setStatusTip('Select file')
-        extractAction.triggered.connect(self.choix_fichier)
-        
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('&File')
-        fileMenu.addAction(extractAction)
-        
-        
         self.afficher_graph()
+        
         self.select_file.clicked.connect(self.choix_fichier)
         self.launch.clicked.connect(self.afficher)
         self.add_point.clicked.connect(self.ajouter_points)
@@ -48,14 +39,25 @@ class InterfaceGraphique(QtWidgets.QMainWindow, Ui_MainWindow):
         #Si un paramètre est changé on met le graphique à jour
         self.add_point.clicked.connect(self.afficher_graph)
         self.remove_point.clicked.connect(self.afficher_graph)
+        
+        self.point0spinBox.valueChanged.connect(self.afficher_graph)
         self.point1spinBox.valueChanged.connect(self.afficher_graph)
         self.point2spinBox.valueChanged.connect(self.afficher_graph)
         self.point3spinBox.valueChanged.connect(self.afficher_graph)
+        self.point5spinBox.valueChanged.connect(self.afficher_graph)
+        
+        self.point0Slider.valueChanged.connect(self.afficher_graph)
         self.point1Slider.valueChanged.connect(self.afficher_graph)
         self.point2Slider.valueChanged.connect(self.afficher_graph)
         self.point3Slider.valueChanged.connect(self.afficher_graph) 
+        self.point5Slider.valueChanged.connect(self.afficher_graph)
         
-        
+        self.point1spinBox.setMinimum(self.point0spinBox.value())
+        self.point1spinBox.setValue(self.point0spinBox.value())
+        self.point2spinBox.setMinimum(self.point1spinBox.value())
+        self.point2spinBox.setValue(self.point1spinBox.value())
+        self.point3spinBox.setMinimum(self.point2spinBox.value())
+        self.point3spinBox.setValue(self.point2spinBox.value())
         
    
   
